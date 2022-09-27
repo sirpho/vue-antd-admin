a3baf047414bda1418d20190e15d5d435daa96d4
sirpho <1060686387@qq.com>
2022-09-27 10:00:59 +0800

    修复展示收起按钮显示不正确的问题

--- src/components/GDesign/ProTable/components/Form/index.tsx
+++ src/components/GDesign/ProTable/components/Form/index.tsx
@@ -462,7 +462,7 @@ export default defineComponent({
                     justifyContent: 'flex-end'
                   },
                   advanced: false,
-                  showAdvanced: formNode.length >= rowLength.value
+                  showAdvanced: formNode.length > rowLength.value
                 })}
             </>
           )
