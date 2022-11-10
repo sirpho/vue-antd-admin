8cfae0c4b884999f3a7201900169246ef8ea2f31
sirpho <1060686387@qq.com>
2022-11-10 09:38:05 +0800

    鉴权bug修改

--- src/hooks/web/usePermissions.ts
+++ src/hooks/web/usePermissions.ts
@@ -4,7 +4,7 @@ import { useStore } from '@gx-vuex'
 import { isString, isArray, isObject } from '@/utils/validate'
 
 export function usePermissions(): {
-  permission: Ref<boolean | object>;
+  permission: Ref<boolean | object>
   hasPermission: (value: object | string | string[]) => void
 } {
   const store = useStore()
@@ -14,10 +14,10 @@ export function usePermissions(): {
 
   function hasPermission(value: object | string | string[]) {
     if (value && isObject(value) && Object.keys(value).length > 0) {
-      Object.keys(value).map(item => {
+      Object.keys(value).map((item) => {
         let isExist = true
         const permissionFlag = value[item]
-        const hasPermissions = permissions.some(el => {
+        const hasPermissions = permissions.some((el) => {
           return all_permission.value === el || permissionFlag.includes(el)
         })
         if (!hasPermissions) {
@@ -27,8 +27,17 @@ export function usePermissions(): {
         return item
       })
     } else if (isString(value) || isArray(value)) {
-      permission.value = permissions.some(el => {
-        return all_permission.value === el || (value as string[]).includes(el)
+      permission.value = permissions.some((el) => {
+        const isAll = all_permission.value === el
+        let permissionTexts
+        if (isString(value)) {
+          permissionTexts = [value]
+        } else {
+          permissionTexts = value
+        }
+
+        const isIncludes = permissionTexts.includes(el)
+        return isAll || isIncludes
       })
     }
   }
