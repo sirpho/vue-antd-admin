47077a9def9f4926e927e9ea79731aab2346381f
sirpho <sirpho@gmail.com>
2024-03-26 17:46:59 +0800

    11

--- src/views/dashboard/workplace/index.vue
+++ src/views/dashboard/workplace/index.vue
@@ -1,12 +1,101 @@
 <template>
   <g-pro-page-container>
-    <div class="workplace-content">
-      <h1>欢迎使用模板系统</h1>
-      <p>welcome to use template management system</p>
+    <!--    <div class="workplace-content">-->
+    <!--      <h1>欢迎使用模板系统</h1>-->
+    <!--      <p>welcome to use template management system</p>-->
+    <!--    </div>-->
+
+    <input type="text" v-model="state.content" />
+
+    <button @click="generateQRCode">生成二维码</button>
+
+    <div class="canvas-wrapper">
+      <canvas class="canvas-content" width="320" height="320" ref="canvasRef"></canvas>
+      <canvas class="canvas-content" width="320" height="390" ref="resultRef"></canvas>
     </div>
   </g-pro-page-container>
 </template>
+<script>
+import QRCode from 'qrcode'
+import { dataURLtoBlob, dataURLtoFile } from '@sirpho/utils/util'
 
+export default {
+  setup() {
+    const state = reactive({
+      content: ''
+    })
+    const canvasRef = ref()
+    const resultRef = ref()
+
+    const generateQRCode = () => {
+      handleQRCode(state.content)
+    }
+    const handleQRCode = (content) => {
+      QRCode.toCanvas(
+        canvasRef.value,
+        content,
+        { width: 320, margin: 0.5 },
+        async function (error) {
+          if (error) return console.error('二维码生成失败', error)
+          nextTick(() => {
+            const targetContext = resultRef.value.getContext('2d')
+            // 设置背景颜色
+            targetContext.fillStyle = '#FFFFFF'
+            // 填充整个 Canvas
+            targetContext.fillRect(0, 0, 320, 390)
+            // 二维码填充
+            targetContext.drawImage(canvasRef.value, 0, 0)
+            // 设置字体样式
+            targetContext.font = '18px 苹方'
+            // 设置文字颜色
+            targetContext.fillStyle = '#000000'
+            // 设置文字居中对齐
+            targetContext.textAlign = 'center'
+            // 设置文字垂直居中对齐
+            targetContext.textBaseline = 'middle'
+            // 计算文字的居中位置
+            const textX = 320 / 2
+            const textY = (390 - 320) / 2 + 320
+            // 绘制填充文字
+            targetContext.fillText(content, textX, textY)
+
+            // 转为blob后下载
+            const dataURL = resultRef.value.toDataURL('image/png')
+            const blob = dataURLtoBlob(dataURL)
+
+            const url = window.URL.createObjectURL(blob)
+
+            const link = document.createElement('a')
+            link.href = url
+            link.download = 'test.png'
+
+            link.click()
+
+            window.URL.revokeObjectURL(url)
+          })
+        }
+      )
+    }
+
+    return {
+      state,
+      canvasRef,
+      resultRef,
+      handleQRCode,
+      generateQRCode
+    }
+  }
+}
+</script>
 <style lang="less" scoped>
 @import './style';
+
+.canvas-wrapper {
+  display: flex;
+  gap: 24px;
+}
+.canvas-content {
+  width: 320px;
+  height: 390px;
+}
 </style>
--- src/views/example/visualization/components/ChartModal.tsx
+++ src/views/example/visualization/components/ChartModal.tsx
@@ -1,168 +1,168 @@
-import { defineComponent, reactive } from 'vue'
-import { Chart } from '@antv/g2'
-import { formatPercentageTime, formatPower, formatTime } from '@/views/example/visualization/utils'
-import { divide, percentage } from '@sirpho/utils/math'
-
-let chartInstance: any
-export default defineComponent({
-  emits: ['handleOk'],
-  setup(_props, { expose, emit }) {
-    const state = reactive({
-      modalTitle: '',
-      visible: false,
-      isFail: false,
-      spinning: false,
-      skeletonLoading: false,
-      disabled: false,
-      pickerVisible: false,
-      historyList: undefined,
-      expireTime: 0,
-      startTime: 0,
-      endTime: 0
-    })
-    /**
-     * 恢复数据到初始化状态
-     */
-    const resetModalState = () => {
-      state.isFail = false
-      state.spinning = false
-      state.visible = false
-      state.skeletonLoading = false
-    }
-
-    /**
-     * 查看
-     */
-    const watch = async (record: any, list: any[], startTime: number, endTime: number) => {
-      state.modalTitle = `查看${record.identifer}`
-      state.skeletonLoading = true
-      state.visible = true
-      state.startTime = startTime
-      state.endTime = endTime
-      const container = renderChart(list)
-      setTimeout(async () => {
-        state.skeletonLoading = false
-        await nextTick()
-        document.getElementById('chart-container').appendChild(container)
-        chartInstance?.render()
-      }, 100)
-    }
-
-    /**
-     * 渲染图表 两个区域图合在一起
-     * @param data
-     */
-    const renderChart = (data) => {
-      chartInstance = new Chart({
-        width: 1152,
-        height: 700
-      })
-      chartInstance
-        .data(data)
-        .axis('x', {
-          labelFormatter: (value) => {
-            return formatTime(value)
-          },
-          line: true,
-          title: '时间'
-        })
-        .axis('y', {
-          labelFormatter: (value) => {
-            return formatPower(value)
-          },
-          line: true,
-          title: false
-        })
-
-      chartInstance
-        .area()
-        .encode('x', (d) => new Date(d.ts))
-        .encode('y', 'hs')
-        .style('fill', 'linear-gradient(-90deg, #2b2d30 0%, #202124 100%)')
-        .tooltip({
-          items: [
-            {
-              channel: 'y',
-              color: '#e2d3ae',
-              name: '实际',
-              valueFormatter: (value) => formatPower(value)
-            },
-            (item) => {
-              return {
-                color: '#ffffff',
-                name: '倍率',
-                value: percentage(divide(item.hs2, item.hs))
-              }
-            }
-          ]
-        })
-
-      chartInstance
-        .area()
-        .encode('x', (d) => new Date(d.ts))
-        .encode('y', 'hs2')
-        .slider({
-          x: {
-            formatter: (value) => {
-              return formatPercentageTime(value, state.startTime, state.endTime)
-            }
-          }
-        })
-        .style('fill', 'linear-gradient(-90deg, #fcebbd 0%, #d6f17f 100%)')
-        .tooltip({
-          items: [{ channel: 'y', name: '结算', valueFormatter: (value) => formatPower(value) }]
-        })
-
-      return chartInstance.getContainer()
-    }
-
-    onUnmounted(() => {
-      chartInstance?.destroy()
-    })
-
-    /**
-     * 关闭弹窗
-     */
-    const handleCancel = () => {
-      resetModalState()
-      emit('handleOk')
-    }
-
-    /**
-     * 暴露组件内部的方法
-     */
-    expose({
-      watch
-    })
-
-    return () => {
-      return (
-        <div>
-          <g-pro-modal
-            title={state.modalTitle}
-            visible={state.visible}
-            isFail={state.isFail}
-            spinning={state.spinning}
-            skeletonLoading={state.skeletonLoading}
-            onCancel={() => handleCancel()}
-            type="normal"
-            width="1200px"
-            v-slots={{
-              footer: () => {
-                return (
-                  <div class="gx-pro-modal-footer">
-                    <a-button key="cancel" disabled={state.spinning} onClick={handleCancel}>
-                      关闭
-                    </a-button>
-                  </div>
-                )
-              }
-            }}
-          >
-            <div id="chart-container"></div>
-          </g-pro-modal>
-        </div>
-      )
-    }
-  }
-})
+// import { defineComponent, reactive } from 'vue'
+// import { Chart } from '@antv/g2'
+// import { formatPercentageTime, formatPower, formatTime } from '@/views/example/visualization/utils'
+// import { divide, percentage } from '@sirpho/utils/math'
+//
+// let chartInstance: any
+// export default defineComponent({
+//   emits: ['handleOk'],
+//   setup(_props, { expose, emit }) {
+//     const state = reactive({
+//       modalTitle: '',
+//       visible: false,
+//       isFail: false,
+//       spinning: false,
+//       skeletonLoading: false,
+//       disabled: false,
+//       pickerVisible: false,
+//       historyList: undefined,
+//       expireTime: 0,
+//       startTime: 0,
+//       endTime: 0
+//     })
+//     /**
+//      * 恢复数据到初始化状态
+//      */
+//     const resetModalState = () => {
+//       state.isFail = false
+//       state.spinning = false
+//       state.visible = false
+//       state.skeletonLoading = false
+//     }
+//
+//     /**
+//      * 查看
+//      */
+//     const watch = async (record: any, list: any[], startTime: number, endTime: number) => {
+//       state.modalTitle = `查看${record.identifer}`
+//       state.skeletonLoading = true
+//       state.visible = true
+//       state.startTime = startTime
+//       state.endTime = endTime
+//       const container = renderChart(list)
+//       setTimeout(async () => {
+//         state.skeletonLoading = false
+//         await nextTick()
+//         document.getElementById('chart-container').appendChild(container)
+//         chartInstance?.render()
+//       }, 100)
+//     }
+//
+//     /**
+//      * 渲染图表 两个区域图合在一起
+//      * @param data
+//      */
+//     const renderChart = (data) => {
+//       chartInstance = new Chart({
+//         width: 1152,
+//         height: 700
+//       })
+//       chartInstance
+//         .data(data)
+//         .axis('x', {
+//           labelFormatter: (value) => {
+//             return formatTime(value)
+//           },
+//           line: true,
+//           title: '时间'
+//         })
+//         .axis('y', {
+//           labelFormatter: (value) => {
+//             return formatPower(value)
+//           },
+//           line: true,
+//           title: false
+//         })
+//
+//       chartInstance
+//         .area()
+//         .encode('x', (d) => new Date(d.ts))
+//         .encode('y', 'hs')
+//         .style('fill', 'linear-gradient(-90deg, #2b2d30 0%, #202124 100%)')
+//         .tooltip({
+//           items: [
+//             {
+//               channel: 'y',
+//               color: '#e2d3ae',
+//               name: '实际',
+//               valueFormatter: (value) => formatPower(value)
+//             },
+//             (item) => {
+//               return {
+//                 color: '#ffffff',
+//                 name: '倍率',
+//                 value: percentage(divide(item.hs2, item.hs))
+//               }
+//             }
+//           ]
+//         })
+//
+//       chartInstance
+//         .area()
+//         .encode('x', (d) => new Date(d.ts))
+//         .encode('y', 'hs2')
+//         .slider({
+//           x: {
+//             formatter: (value) => {
+//               return formatPercentageTime(value, state.startTime, state.endTime)
+//             }
+//           }
+//         })
+//         .style('fill', 'linear-gradient(-90deg, #fcebbd 0%, #d6f17f 100%)')
+//         .tooltip({
+//           items: [{ channel: 'y', name: '结算', valueFormatter: (value) => formatPower(value) }]
+//         })
+//
+//       return chartInstance.getContainer()
+//     }
+//
+//     onUnmounted(() => {
+//       chartInstance?.destroy()
+//     })
+//
+//     /**
+//      * 关闭弹窗
+//      */
+//     const handleCancel = () => {
+//       resetModalState()
+//       emit('handleOk')
+//     }
+//
+//     /**
+//      * 暴露组件内部的方法
+//      */
+//     expose({
+//       watch
+//     })
+//
+//     return () => {
+//       return (
+//         <div>
+//           <g-pro-modal
+//             title={state.modalTitle}
+//             visible={state.visible}
+//             isFail={state.isFail}
+//             spinning={state.spinning}
+//             skeletonLoading={state.skeletonLoading}
+//             onCancel={() => handleCancel()}
+//             type="normal"
+//             width="1200px"
+//             v-slots={{
+//               footer: () => {
+//                 return (
+//                   <div class="gx-pro-modal-footer">
+//                     <a-button key="cancel" disabled={state.spinning} onClick={handleCancel}>
+//                       关闭
+//                     </a-button>
+//                   </div>
+//                 )
+//               }
+//             }}
+//           >
+//             <div id="chart-container"></div>
+//           </g-pro-modal>
+//         </div>
+//       )
+//     }
+//   }
+// })
